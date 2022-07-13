782e13f5969e1fc0955919b53ad3816be420e3c7
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-13 12:11:33 +0530

    Create main.dart

---
+++ L1_Hello World/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myappw());
+}
+
+class Myappw extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("YOOOOOOOOO"),
+    );
+  }
+}

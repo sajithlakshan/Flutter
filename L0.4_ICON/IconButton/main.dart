9d925987048e5dc34865c8e5586268b2e7730bd0
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-15 22:43:52 +0530

    Create main.dart

---
+++ L0.4_ICON/IconButton/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app Relawa"),
+          centerTitle: true,
+          backgroundColor: Colors.amber[900],
+        ),
+
+
+        body: Center(
+            child: IconButton(
+          onPressed: () {
+            print('sofiya');
+          },
+          icon: Icon(Icons.mail),
+          color: Color.fromARGB(255, 240, 6, 6),
+        )),
+
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click"),
+          backgroundColor: Color.fromARGB(255, 2, 255, 14),
+        ),
+      ),
+    );
+  }
+}

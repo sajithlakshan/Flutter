2881e6cf35da807bc38dccdf08c5e0ec324e1426
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-16 01:57:33 +0530

    Create main.dart

---
+++ L0.6_Padding/Padding Verticle and Horizontal/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app Relawa"),
+          centerTitle: true,
+          backgroundColor: Colors.amber[900],
+        ),
+
+        
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 12.0),
+          color: Color.fromARGB(255, 214, 7, 255),
+          child: Text('Hello'),
+        ),
+
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click"),
+          backgroundColor: Color.fromARGB(255, 2, 255, 14),
+        ),
+      ),
+    );
+  }
+}

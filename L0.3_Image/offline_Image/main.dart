227e7ff23bb9db3844643cc5e69fee9be558ac9b
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-15 17:03:21 +0530

    Create main.dart

---
+++ L0.3_Image/offline_Image/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app Relawa"),
+          centerTitle: true,
+          backgroundColor: Colors.amber[900],
+        ),
+
+
+        body: Center(
+            child: Image(
+          image: AssetImage('A_assets/phone.jpg'),
+        )),
+
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click"),
+          backgroundColor: Color.fromARGB(255, 2, 255, 14),
+        ),
+      ),
+    );
+  }
+}

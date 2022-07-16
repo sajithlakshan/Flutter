52777780454151806a98c4214df198261565653b
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-16 11:32:11 +0530

    Create main.dart

---
+++ L0.7_Margin/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app Relawa"),
+          centerTitle: true,
+          backgroundColor: Colors.amber[900],
+        ),
+
+
+        body: Container(
+          padding: EdgeInsets.fromLTRB(10, 50, 100, 150),
+          margin: EdgeInsets.all(40.0),
+          color: Color.fromARGB(255, 214, 7, 255),
+          child: Text('Hello'),
+        ),
+
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click"),
+          backgroundColor: Color.fromARGB(255, 2, 255, 14),
+        ),
+      ),
+    );
+  }
+}

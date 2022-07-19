fdc1659143ffbaf5c23921a845dd234a71615f08
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-19 19:28:45 +0530

    Create sod.dart

---
+++ L0.12_List/Simple/sod.dart
@@ -0,0 +1,9 @@
+class WoNUM {
+  String Vellage_name = '';
+  String Power_Level = '';
+
+  WoNUM(String power, String Vname) {
+    this.Vellage_name = Vname;
+    this.Power_Level = power;
+  }
+}

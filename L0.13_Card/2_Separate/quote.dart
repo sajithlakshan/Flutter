fee72a70983d8f2af3f6e78b73b50334dc0857f8
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-07-20 19:02:33 +0530

    Update quote.dart

--- L0.13_Card/2_Separate/quote.dart
+++ L0.13_Card/2_Separate/quote.dart
@@ -1,17 +1,17 @@
-class Quote {
+class SAJI {
   String text1 = '';
   String author2 = '';
 
   //  normal constructor, as we've already seen
 
-  //  Quote(String author, String text){
+  //  SAJI(String author, String text){
   //    this.text = text;
   //    this.author = author;
   //  }
 
   //  constructor with named parameters
 
-  //  Quote({ String author, String text }){
+  //  SAJI({ String author, String text }){
   //    this.text = text;
   //    this.author = author;
   //  }
@@ -19,7 +19,7 @@ class Quote {
   // constructor with named parameters
   // & automatically assigns named arguments to class properties
 
-  Quote({required String text1, required String author}) {
+  SAJI({required String text1, required String author}) {
     this.text1 = text1;
     this.author2 = author;
   }

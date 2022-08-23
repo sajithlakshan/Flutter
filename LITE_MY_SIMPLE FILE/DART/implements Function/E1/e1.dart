1b180bbf84fc574dcbcc1be3660de3f291107b93
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-08-23 23:13:43 +0530

    Add files via upload

---
+++ LITE_MY_SIMPLE FILE/DART/implements Function/E1/e1.dart
@@ -0,0 +1,9 @@
+import 'dart:ffi';
+
+String Lakshab(String name) {
+  return 'YOOOOOOOOOO ' + name;
+}
+
+String Shakabun(String Shammi, String daras) {
+  return 'Sakibara ' + Shammi + " " + daras;
+}

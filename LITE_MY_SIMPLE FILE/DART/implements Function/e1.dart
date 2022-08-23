3e759dbf1f71970c7fba4288eed9c710a51bf477
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-08-23 23:10:16 +0530

    Delete LITE_MY_SIMPLE FILE/DART/implements Function directory

--- LITE_MY_SIMPLE FILE/DART/implements Function/e1.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ffi';
-
-String Lakshab(String name) {
-  return 'YOOOOOOOOOO ' + name;
-}
-
-String Shakabun(String Shammi, String daras) {
-  return 'Sakibara ' + Shammi + " " + daras;
-}

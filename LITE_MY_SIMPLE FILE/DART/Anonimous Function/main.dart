e39e9b7e9f60327f36c4e23112774601024be5ff
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-08-26 22:07:40 +0530

    Add files via upload

---
+++ LITE_MY_SIMPLE FILE/DART/Anonimous Function/main.dart
@@ -0,0 +1,24 @@
+import 'e1.dart';
+
+void main() {
+  var list = ["John", "Doe", "Smith", "Alex"];
+  list.forEach((item) {
+    print('${list.indexOf(item)}: $item');
+  });
+  print("---------------------------------------------");
+  final numbers = <int>[1, 2, 3, 5, 6, 7];
+  var result = numbers.where((x) {
+    return x < 5;
+  }); // (1, 2, 3)
+  print(result);
+  print("---------------------------------------------");
+  result = numbers.where((x) => x > 5);
+  print(result.toList());
+  /*
+  final suggestions = allBooks.where((Dbooks) {
+    final BookTitle = Dbooks.title.toLowerCase();
+  }).toList();
+
+  List<COMP> books = allBooks;
+  */
+}

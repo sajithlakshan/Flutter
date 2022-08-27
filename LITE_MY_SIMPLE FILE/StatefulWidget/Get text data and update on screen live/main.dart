428aed8bcb871c4f30bd3fef95e73a25453086e1
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-08-27 22:46:16 +0530

    Add files via upload

---
+++ LITE_MY_SIMPLE FILE/StatefulWidget/Get text data and update on screen live/main.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      title: 'Retrieve Text Input',
+      home: MyCustomForm(),
+    ));
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  var EERI;
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start listening to changes.
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _printLatestValue() {
+    print('Second text field: ${myController.text}');
+    setState(() {
+      EERI = myController.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Retrieve Text Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text('First text field:  ${myController.text}'),
+            TextField(
+              controller: myController,
+            ),
+            Text('Second text field:  ${myController.text}'),
+            Text('Thered text field: ${EERI}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
